63fb5e04039747fc3229279e166e3e0cb8f40ed8
lenahmbae <lenahmbae22@gmail.com>
2025-03-25 12:04:55 +0300

    adds loops

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function countDown(number) {
+   
+    while (number >= 0) {
+    
+      console.log(number);
+      
+      
+      number -= 1;
+    }
+  } 
+  
+  countDown(10);
+
+  function writeCards(names, event) {
+    let messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages; 
+
+  }
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
